503585ba2b8d5d54d311329e2928ac101b026cf2
Irving2355 <it235597@gmail.com>
2026-04-13 11:40:33 -0600

    Clase 13-04-2026

--- Parcial 3/Practica-3/src/componentes/AsyncDemo.jsx
+++ Parcial 3/Practica-3/src/componentes/AsyncDemo.jsx
@@ -1,23 +1,69 @@
 import React from 'react'
 
+import {
+    obtenerCurso,
+    obtnerProfesor,
+    obtnerMateria
+} from '../serivces/fakeService'
+
 function AsyncDemo() {
+    const caragarCursosConThen = () =>{
+        obtenerCurso().then((resultado) =>{
+            console.log('Curso recibido con then ', resultado)
+            alert(
+                'Curso: ' + resultado.nombre +
+                '\nDuracion: '+resultado.duracion+
+                '\nModalidad: '+resultado.modalidad
+            )
+        }).catch((error) =>{
+            console.log('Error: ', error)
+        })
+    }
+
+    const cargarProfesorConAwait = async () =>{
+        try{
+            const resultado = await obtnerProfesor()
+            console.log('Profesor recibido')
+            alert(
+                'Profesor: '+ resultado.nombre +
+                '\nArea: ' + resultado.area 
+            )
+        } catch(error){
+            console.log('Error: ', error)
+        }
+    }
+
+    const caragarMateriaConAwait = async () =>{
+        try{
+            const resultado = await obtnerMateria()
+            console.log('Materia recibida')
+            alert(
+                'Materia: '+ resultado.nombre +
+                '\nSemestre: ' + resultado.area 
+            )
+        }catch(error){
+            console.log('Error: ',error)
+        }
+    }
   return (
-    <div>AsyncDemo</div>
+    <div>
+        <h2>Promesas y async/await</h2>
+        <p>Presiona los botones para obtener los datos</p>
+
+        <button onClick={caragarCursosConThen}>
+            Caragar Cursos
+        </button>
+
+        <button onClick={cargarProfesorConAwait}>
+            Caragar Profesor
+        </button>
+
+        <button onClick={caragarMateriaConAwait}>
+            Cargar Materia
+        </button>
+    </div>
   )
 }
 
-function obtenerMensaje(){
-    return new Promise((resolve) =>{
-        setTimeout(() =>{
-            resolve('Hola desde una promesa')
-        }, 2000) 
-    })
-}
-
-obtenerMensaje().then(resultado =>{
-    console.log(resultado)
-}).catch((error) =>{
-    console.log(error)
-})
 
 export default AsyncDemo
